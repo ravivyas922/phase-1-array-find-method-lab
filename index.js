d75e702655c92e5de9f03da2bcb4c8c90e593870
Ravi <ravivyas922@gmail.com>
2022-11-30 12:20:07 -0500

    commit tree 4dedb9d71c5f5b5ee795d05e1bd79c2b0df8ff82

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(record)
+{
+    
+    let array = record.find(season => season.result === "W");
+    if(array == undefined)
+    {
+        return undefined;
+    }
+    else
+    {
+        return array.year;
+    }
+
+    
+}
