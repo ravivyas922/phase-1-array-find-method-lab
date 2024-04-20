d5850bb05f5e8a912b9419cbd9f962336c9d4371
Ravi <ravivyas922@gmail.com>
2024-04-19 20:14:24 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    const win = record.find(thing => thing.result === "W");
+    if(win !== undefined){
+        return win.year; 
+    }
+    else{return undefined;}
+}
+
+
